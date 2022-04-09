2f903ff67e5cf451b7c39198047d09813a9e5302
maxhirtens <max@maxhirtenstein.com>
2022-04-09 14:02:07 -0700

    UI works correctly

--- calculator.js
+++ calculator.js
@@ -24,15 +24,18 @@ function setupIntialValues() {
   const amount = document.getElementById("loan-amount");
   const years = document.getElementById("loan-years");
   const rate = document.getElementById("loan-rate");
-  amount.value = '$10,000';
+  amount.value = '10000';
   years.value = '5';
-  rate.value = '3%';
+  rate.value = '3';
   update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const monthlyPayment = calculateMonthlyPayment(getCurrentUIValues());
+  const monthlyPaymentOutput = document.getElementById('monthly-payment');
+  monthlyPaymentOutput.innerText = `$${monthlyPayment}`;
 }
 
 // Given an object of values (a value has amount, years and rate ),
@@ -41,10 +44,10 @@ function update() {
 function calculateMonthlyPayment(values) {
   const monthlyRate = (values.rate / 100) / 12;
   const n = values.years * 12;
-  return (values.amount * monthlyRate) / (1 - Math.pow(1+monthlyRate), -n);
+  return ((values.amount * monthlyRate) / (1 - Math.pow(1+monthlyRate, -n))).toFixed(2);
 }
 
-// Given a string representing the monthly payment value,
-// update the UI to show the value.
-function updateMonthly(monthly) {
-}
+// // Given a string representing the monthly payment value,
+// // update the UI to show the value.
+// function updateMonthly(monthly) {
+// }
